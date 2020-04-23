dc57d1ca59bc045065e4e42db3a9c306f76f9268
Tomtom-dev <thomasheddadji0@gmail.com>
2020-04-23 14:30:35 +0200

    last update

---
+++ index.js
@@ -0,0 +1,9 @@
+const toTitleCase = require('to-title-case');
+
+// console.log(toTitleCase);
+
+const lower = 'The lord of the rings: the return of the king';
+
+const title = toTitleCase(lower);
+
+console.log(title)
